afcdc758422c6f3697695b9af4a003e9eef06b73
Hamid <hamidnemati@aut.ac.ir>
2020-12-04 07:28:09 +0330

    join and left handled. ready for rooms and file transder

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_socketio import SocketIO
 import sqlite3
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+
+clients = []
+
 # creating databse and table if users
 db = sqlite3.connect('messenger.db',check_same_thread=False)
-db.execute('CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, fname TEXT NOT NULL, lname TEXT NOT NULL)')
+db.execute('DROP TABLE IF EXISTS users')
+db.execute('DROP TABLE IF EXISTS groups')
+db.execute('CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT,socketid TEXT NOT NULL, fname TEXT NOT NULL, lname TEXT NOT NULL)')
 db.execute('CREATE TABLE IF NOT EXISTS groups(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL)')
+
+room = 'general'
 
 def query_db(query, args=(), one=False):
     cur = db.execute(query, args)
@@ -19,7 +26,10 @@ def query_db(query, args=(), one=False):
 
 @app.route('/')
 def welcome():
-    query_db('insert into groups(name) values("general")')
+    
+    if (query_db('select * from groups where name = ?',args=["general"],one=True) is None):
+        query_db('insert into groups(name) values(?)',args=["general"])
+        print('a group made')
     return render_template('Start.html')
 
 
@@ -43,9 +53,8 @@ def CheckUsername(methods=['GET', 'POST']):
     name_exist = query_db('select * from users where fname=? and lname=?',[first_name, last_name], one=True)
     print(name_exist)
     
-    if (name_exist == None):
-         query_db('INSERT INTO users(fname, lname) VALUES(?, ?)', args=[first_name, last_name])
-         return render_template('session.html')
+    if (name_exist is None):
+         return render_template('session.html',fname = first_name , lname = last_name, room = room )
     else:
          duplicate=True
          return render_template('Username.html', duplicate = duplicate)
@@ -58,6 +67,7 @@ def CheckUsername(methods=['GET', 'POST']):
 
 @app.route('/chatroom')
 def sessions():
+    
     return render_template('session.html')
 
 
@@ -68,19 +78,41 @@ def messageReceived(methods=['GET', 'POST']):
 
 @socketio.on('my event')
 def handle_my_custom_event(json, methodes=['GET', 'POST']):
+    
+    name = (json.get('user_name'))
+    id = str(json.get('id'))
+    
+    if (id not in clients and id is not None and name is not None):
+        print('new user : {} '.format(name))
+        first_name, last_name = name.split()
+        clients.append(id)
+        query_db('INSERT INTO users(socketid, fname, lname) VALUES(?, ?, ?)', args=[id, first_name, last_name])
+    
     print('received my event: ' + str(json))
-    socketio.emit('my response', json, callback=messageReceived)
+    if (json.get('data') is not None):
+        json.update({'user_name':name+' joined ...'})
+        json.update({'message':''})
+        socketio.emit('my response', json, callback=messageReceived , broadcast=True)
+    else:
+        socketio.emit('my response', json, callback=messageReceived , room = room)
 
-@socketio.on('connect')
-def test_connect():
-    socketio.emit('my response', {'data': 'Connected'})
+
+
 
 
 
 @socketio.on('disconnect')
-def test_disconnect():
-    socketio.emit('my response' ,'')
-    print('Client disconnected')
+def test_disconnect(methodes=['GET', 'POST']):
+    if (request.sid in clients):
+        
+        clients.remove(request.sid)
+        client = query_db('SELECT * FROM users WHERE socketid=?', args=[request.sid])
+        name = client[0][2]+" "+client[0][3]+" has left..."
+        # name = fname+" "+lname+" has left..."
+        json = {'user_name':name , 'message':''}
+        socketio.emit('my response', json, callback=messageReceived , broadcast=True)
+        query_db('DELETE FROM users WHERE socketid=?',args=[request.sid])
+        print('{} is disconnected'.format(request.sid))
 
 
 
